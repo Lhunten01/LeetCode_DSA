5c8eae7e49083ca30e6c7258e5ec182b8e7f0047
Lhunten01 <120653962+Lhunten01@users.noreply.github.com>
2023-10-25 15:13:21 +0530

    Time: 0 ms (100%), Space: 6.5 MB (12.89%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.cpp
+++ 2050-count-good-numbers/2050-count-good-numbers.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-long long modulo = 1000000007; 
-     long long pow(long long base, long long exp) {
-        if (exp == 0) return 1;
-        long long halfPower = pow(base, exp / 2) % modulo;
-        long long result = (halfPower * halfPower) % modulo;
-
-        if (exp % 2 == 1) {
-            result = (result * base) % modulo;
-        }
-
-        return result;
+long long mod = 1000000007; 
+     long long pow(long long x, long long y) {
+       if(y==0)
+       return 1;
+       long long ans=pow(x,y/2);
+       ans*=ans;
+       ans%=mod;
+       if(y%2)
+       ans*=x;
+       ans%=mod; 
+        return ans;
     }
     int countGoodNumbers(long long n) {
 
         long long oddIndexes=n/2;
         long long evenIndexes=n/2+n%2;
-        return (pow(5,evenIndexes)*pow(4,oddIndexes))%modulo;
+        return (pow(5,evenIndexes)*pow(4,oddIndexes))%mod;
     }
 };
