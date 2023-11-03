6bc6562c4d64cdae03e84a8ed8bf034b83817e3f
Tenzin Lhundup <120653962+Lhunten01@users.noreply.github.com>
2023-11-03 08:07:38 +0530

    Time: 118 ms (98.62%), Space: 11.8 MB (16.21%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n<=2)
+        return 0;
+        bool prime[n+1];
+         memset(prime, true, sizeof(prime));
+        for(int p=2;p*p<n;p++)
+        {
+            if(prime[p])
+            {
+                for(int i=p*p;i<n;i+=p)
+                {
+                    prime[i]=false;
+                }
+            }
+        }
+        int count=0;
+        for(int i=2;i<n;i++)
+        {
+            if(prime[i])
+            count++;
+        }
+        return count;
+    }
+};
