c904732c6a1008b04f4cc3b265a4a0cd6534e2e0
Tenzin Lhundup <120653962+Lhunten01@users.noreply.github.com>
2024-06-02 11:26:42 +0530

    Time: 0 ms (100%), Space: 7.8 MB (16.87%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -14,6 +14,21 @@ public:
      }
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-       return numPath(m-1,n-1,dp);
+        
+         for(int i=0;i<m;i++)
+         {
+            for(int j=0;j<n;j++)
+            { if(i==0&&j==0)
+                { 
+                 dp[i][j]=1;
+                 continue;
+                 }
+                int top=0,left=0;
+                 if(i>0) top=dp[i-1][j];
+                 if(j>0) left=dp[i][j-1];
+                 dp[i][j]=top+left;
+            }
+         }
+         return dp[m-1][n-1];
     }
 };
