20f7d9065c3ffe5d818b5463730f591ad866d278
Tenzin Lhundup <120653962+Lhunten01@users.noreply.github.com>
2024-07-07 11:18:00 +0530

    Time: 81 ms (88.5%), Space: 60.8 MB (92.54%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-         set<int>st;
-         for(int i=0;i<nums.size();i++)
+         sort(nums.begin(),nums.end());
+         int n=nums.size();
+         for(int i=1;i<n;i++)
          {
-            if(st.find(nums[i])!=st.end())
-            return true;
-            st.insert(nums[i]);
+              if(nums[i-1]==nums[i])
+              return true;
          }
-         
          return false;
     }
 };
