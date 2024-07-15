92482ce15d1c0a365ecc3898da7069d49c46fdb5
Tenzin Lhundup <120653962+Lhunten01@users.noreply.github.com>
2024-07-15 10:25:49 +0530

    Time: 15 ms (51.76%), Space: 20.8 MB (26.72%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int x=(n*(n+1))/2;
+        cout<<x<<" ";
+        int sum=accumulate(nums.begin(),nums.end(),0);
+          return x-sum;
+    }
+};
