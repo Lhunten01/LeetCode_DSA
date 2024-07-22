bbaf28f9858e49885e73fdde2ca201ae74094c3b
Tenzin Lhundup <120653962+Lhunten01@users.noreply.github.com>
2024-07-22 20:06:15 +0530

    Time: 18 ms (88.62%), Space: 23.1 MB (28.48%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,37 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        stack<pair<int,int>>st;
-        sort(intervals.begin(), intervals.end());
-        st.push({intervals[0][0],intervals[0][1]});
-        vector<vector<int>>ans;
         int n=intervals.size();
-        for(int i=1;i<n;i++)
+        vector<vector<int>>ans;
+        sort(intervals.begin(),intervals.end());
+        for(int i=0;i<n;i++)
         {
-            int x=intervals[i][0];
-            int y=intervals[i][1];
-            pair<int,int>p=st.top();
-            int low=p.first;
-            int high=p.second;
-           
-            if(x<=high)
-            {  st.pop();
-               st.push({low,max(y,high)});  
-            }
+            if(ans.empty()||ans.back()[1]<intervals[i][0])
+                ans.push_back(intervals[i]);
             else
-           {
-            st.push({x,y});
-           }
+             ans.back()[1]=max(ans.back()[1],intervals[i][1]);
         }
-        while(!st.empty())
-        {
-           auto it=st.top();
-           vector<int>sol;
-           sol={it.first,it.second};
-           ans.push_back(sol);
-           st.pop();
-        }
-        // reverse(ans.begin(), ans.end());
         return ans;
     }
 };
