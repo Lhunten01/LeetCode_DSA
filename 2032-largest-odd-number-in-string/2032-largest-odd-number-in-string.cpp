cabf88618bcbed51e3fa018c08fa8a4031a33e02
Tenzin Lhundup <120653962+Lhunten01@users.noreply.github.com>
2024-07-26 18:20:20 +0530

    Time: 26 ms (70.54%), Space: 16.6 MB (22.14%) - LeetHub

---
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        int n=num.size();
+        int i=n-1;
+        while(i>=0)
+        {
+            if((int)(num[i])%2==1)
+            return num.substr(0,i+1);
+            i--;
+        }
+        return "";
+    }
+};
