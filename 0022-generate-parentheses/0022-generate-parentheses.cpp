23b00d865acdc6f8d85c3f9d16845e53c9bc28a1
Tenzin Lhundup <120653962+Lhunten01@users.noreply.github.com>
2024-08-12 20:50:49 +0530

    Time: 0 ms (100%), Space: 16.2 MB (15.54%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    vector<string>valid;
-void generate(string &s,int open,int close)
-{
-if(open==0&&close==0)
-{
-    valid.push_back(s);
-    return;
-}
-if(open>0)
-{
-    s.push_back('(');
-    generate(s,open-1,close);
-    s.pop_back();
-}
-if(close>0)
-{   if(open<close){
-    s.push_back(')');
-    generate(s,open,close-1);
-    s.pop_back();
-    } 
-}
-
-}
+  void findparenthesis(string s,int open,int close,vector<string>&ans)
+  {
+    if(open==0&&close==0)
+    {
+        ans.push_back(s);
+        return;
+    }
+    if(open>0)
+    {
+        s+='(';
+        findparenthesis(s,open-1,close,ans);
+        s.pop_back();
+    }
+    if(close>0)
+    {
+        if(open<close)
+        {
+            s+=')';
+        findparenthesis(s,open,close-1,ans);
+        s.pop_back();
+        }
+    }
+  }
     vector<string> generateParenthesis(int n) {
-        string s;
-        generate(s,n,n);
-        return valid;
+        string s="";
+        vector<string>ans;
+        findparenthesis(s,n,n,ans);
+        return ans;
     }
 };
