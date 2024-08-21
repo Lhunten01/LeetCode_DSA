38cef791c4e0e05c5e0b0c82cc1466e9766ca3c4
Tenzin Lhundup <120653962+Lhunten01@users.noreply.github.com>
2024-08-21 12:00:32 +0530

    Time: 3 ms (49.98%), Space: 8 MB (73.45%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
-  void sum3(int index,int k,int n,vector<int>temp,set<vector<int>>&st)
-  {
-      if(temp.size()==k||index>=10)
-      {
-          int x=accumulate(temp.begin(),temp.end(),0);
-          if(x==n&&temp.size()==k)
-          {
-              sort(temp.begin(),temp.end());
-              st.insert(temp);
-              return;
-          }
-          else
-          {
-              return;
-          }
-      }
-      sum3(index+1,k,n,temp,st);
-      temp.push_back(index);
-      sum3(index+1,k,n,temp,st);
-  }
+    void findCombination(int index,int k,int n,vector<vector<int>>&ans,vector<int>&temp)
+    {
+        if(n==0&&temp.size()==k)
+        {
+            ans.push_back(temp);
+            return;
+        }
+        for(int i=index;i<10;i++)
+        {  if(i==index)
+           continue;
+            if(i<=n)
+            {temp.push_back(i);
+            findCombination(i,k,n-i,ans,temp);
+            temp.pop_back(); } 
+        }
+    }
     vector<vector<int>> combinationSum3(int k, int n) {
-        set<vector<int>>st;
-        vector<int>temp;
-         sum3(1,k,n,temp,st);
        vector<vector<int>>ans;
-       ans.insert(ans.begin(),st.begin(),st.end());
+       vector<int>temp;
+       findCombination(0,k,n,ans,temp);
        return ans;
     }
 };
