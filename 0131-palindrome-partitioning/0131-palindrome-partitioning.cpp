ac10b621aa3b6b15eadae8dfe4a00777ab4dcb6c
Tenzin Lhundup <120653962+Lhunten01@users.noreply.github.com>
2024-08-22 00:19:16 +0530

    Time: 180 ms (14.62%), Space: 148.4 MB (15.38%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+     bool checkPalindrome(int i,int j,string s)
+     {
+         while(i<=j)
+         {
+            if(s[i]!=s[j])
+            return false;
+            i++;
+            j--;
+         }
+        return true; 
+     }
+     void givePalindrome(int index,string s,vector<vector<string>>&sol,vector<string>temp)
+     {
+        if(index==s.length())
+        {  if(temp.size()>0)
+           { 
+            sol.push_back(temp);
+           }
+           return;
+        }
+        for(int i=index;i<s.length();i++)
+        {
+            if(checkPalindrome(index,i,s))
+            {  
+                string s1=s.substr(index,i-index+1);
+                temp.push_back(s1);
+                givePalindrome(i+1,s,sol,temp);
+                temp.pop_back();
+            }
+        }
+
+     }
+
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>>sol;
+        vector<string>temp;
+       givePalindrome(0,s,sol,temp);
+       return sol;
+    }
+};
