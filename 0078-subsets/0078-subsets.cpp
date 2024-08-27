c53fedfa7cd6997279d26e3055bce1edb62ec55c
Tenzin Lhundup <120653962+Lhunten01@users.noreply.github.com>
2024-08-27 23:07:14 +0530

    Time: 4 ms (39.44%), Space: 8.7 MB (52.25%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-     void subsets(int i,vector<vector<int>>&ans,vector<int>&col,vector<int>&nums)
-     {
-        if(i==nums.size())
-       { ans.push_back(col);
-       return ;
-       }
-       subsets(i+1,ans,col,nums);
-       
-       col.push_back(nums[i]);
-       subsets(i+1,ans,col,nums);
-       col.pop_back();
-     }
     vector<vector<int>> subsets(vector<int>& nums) {
-      vector<vector<int>>ans;
-      vector<int>col;
-      subsets(0,ans,col,nums);
-      return ans;
+        vector<vector<int>>sol;
+        int n=nums.size();
+        for(int i=0;i<pow(2,n);i++)
+        {
+           vector<int>temp;
+           for(int j=0;j<n;j++)
+           {
+             if(i&1<<j)
+              temp.push_back(nums[j]);
+           }
+           sol.push_back(temp);
+        }
+        return sol;
     }
 };
